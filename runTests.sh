3e68538c794c6c36a4342cdee06d1386ae6b348e
benbii <alittlesheep777@gmail.com>
2025-08-13 21:21:46 +0800

    super original readme with no ai usage whatsoever

--- runTests.sh
+++ runTests.sh
@@ -6,11 +6,9 @@ mkdir -p build
 cmake -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release \
   -S. -Bbuild -DCMAKE_C_COMPILER=clang
 cmake --build build
-if ! test -z "$1"; then
-  U=~/.local/upmem-2024.2.0-Linux-x86_64/upmem_env.sh
-  if [ -z "$2" ]; then U="$2"; fi
+if test -n "$1"; then
   mkdir -p devApp/{bins,objdumps}
-  source ~/.local/upmem-2024.2.0-Linux-x86_64/upmem_env.sh
+  source "$1"
   for S in BS GEMV HST-L HST-S MLP OPDEMO OPDEMOF \
       RED SCAN-RSS SCAN-SSA SEL TRNS TS UNI VA; do
     dpu-upmem-dpurte-clang -O3 devApp/$S.c -DNR_TASKLETS=16 \
@@ -21,38 +19,18 @@ if ! test -z "$1"; then
   done
 fi
 
-# if numactl --cpubind 1 true 2>/dev/null; then
-#   time numactl -m0 -N0 build/dmmOpdemo 131072 256 devApp/objdumps/OPDEMO.objdump 4 &
-#   time numactl -m1 -N1 build/dmmBs 5242880 640 devApp/objdumps/BS.objdump
-#   time numactl -m1 -N1 build/dmmGemv 8192 2048 devApp/objdumps/GEMV.objdump
-#   wait
-#   time numactl -m0 -N0 build/dmmTrns 2000 200 devApp/objdumps/TRNS.objdump &
-#   time numactl -m1 -N1 build/dmmHstl 2621440 1280 devApp/objdumps/HST-L.objdump
-#   time numactl -m1 -N1 build/dmmHsts 2621440 1280 devApp/objdumps/HST-S.objdump
-#   time numactl -m1 -N1 build/dmmMlp 1024 1024 devApp/objdumps/MLP.objdump
-#   time numactl -m1 -N1 build/dmmRed 5242880 2560 devApp/objdumps/RED.objdump
-#   time numactl -m1 -N1 build/dmmScanssa 3276800 1600 devApp/objdumps/SCAN-SSA.objdump
-#   time numactl -m1 -N1 build/dmmScanrss 3276800 1600 devApp/objdumps/SCAN-RSS.objdump
-#   time numactl -m1 -N1 build/dmmSel 5242880 2560 devApp/objdumps/SEL.objdump
-#   time numactl -m1 -N1 build/dmmTs 327680 320 devApp/objdumps/TS.objdump
-#   time numactl -m1 -N1 build/dmmUni 100000 256 devApp/objdumps/UNI.objdump
-#   time numactl -m1 -N1 build/dmmVa 5242880 2560 devApp/objdumps/VA.objdump
-#   wait
-#
-# else
-  time build/dmmBs 5242880 640 devApp/objdumps/BS.objdump
-  time build/dmmGemv 8192 2048 devApp/objdumps/GEMV.objdump
-  time build/dmmHstl 2621440 1280 devApp/objdumps/HST-L.objdump
-  time build/dmmHsts 2621440 1280 devApp/objdumps/HST-S.objdump
-  time build/dmmMlp 1024 1024 devApp/objdumps/MLP.objdump
-  time build/dmmRed 5242880 2560 devApp/objdumps/RED.objdump
-  time build/dmmScanssa 3276800 1600 devApp/objdumps/SCAN-SSA.objdump
-  time build/dmmScanrss 3276800 1600 devApp/objdumps/SCAN-RSS.objdump
-  time build/dmmSel 5242880 2560 devApp/objdumps/SEL.objdump
-  time build/dmmTrns 2000 200 devApp/objdumps/TRNS.objdump
-  time build/dmmTs 327680 320 devApp/objdumps/TS.objdump
-  time build/dmmUni 100000 256 devApp/objdumps/UNI.objdump
-  time build/dmmVa 5242880 2560 devApp/objdumps/VA.objdump
-  time build/dmmOpdemo 131072 256 devApp/objdumps/OPDEMO.objdump 3
-  time build/dmmOpdemof 131072 256 devApp/objdumps/OPDEMOF.objdump 3
-# fi
+time build/dmmBs 5242880 640 devApp/objdumps/BS.objdump
+time build/dmmGemv 8192 2048 devApp/objdumps/GEMV.objdump
+time build/dmmHstl 2621440 1280 devApp/objdumps/HST-L.objdump
+time build/dmmHsts 2621440 1280 devApp/objdumps/HST-S.objdump
+time build/dmmMlp 1024 1024 devApp/objdumps/MLP.objdump
+time build/dmmRed 5242880 2560 devApp/objdumps/RED.objdump
+time build/dmmScanssa 3276800 1600 devApp/objdumps/SCAN-SSA.objdump
+time build/dmmScanrss 3276800 1600 devApp/objdumps/SCAN-RSS.objdump
+time build/dmmSel 5242880 2560 devApp/objdumps/SEL.objdump
+time build/dmmTrns 2000 200 devApp/objdumps/TRNS.objdump
+time build/dmmTs 327680 320 devApp/objdumps/TS.objdump
+time build/dmmUni 100000 256 devApp/objdumps/UNI.objdump
+time build/dmmVa 5242880 2560 devApp/objdumps/VA.objdump
+time build/dmmOpdemo 131072 256 devApp/objdumps/OPDEMO.objdump 3
+time build/dmmOpdemof 131072 256 devApp/objdumps/OPDEMOF.objdump 3
